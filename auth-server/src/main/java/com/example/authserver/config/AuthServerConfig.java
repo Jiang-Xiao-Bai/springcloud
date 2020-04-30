386b44e21bc50227dacd5ed5abf3f8d78e6fab6a
Jiang-Xiao-Bai <1536907789@qq.com>
2020-04-30 16:23:55 +0800

    登录认证

---
+++ auth-server/src/main/java/com/example/authserver/config/AuthServerConfig.java
@@ -0,0 +1,33 @@
+package com.example.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * @author zhaohf
+ * @date 2020/4/30 16:11
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("javaboy")
+                .secret(passwordEncoder.encode("123"))
+                .autoApprove(true)
+                .redirectUris("http://localhost:1112/login", "http://localhost:1113/login")
+                .scopes("user")
+                .accessTokenValiditySeconds(7200)
+                .authorizedGrantTypes("authorization_code");
+
+    }
+}
