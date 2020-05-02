1ad0df0f0fb18c8f73591b4abe4905ea5eefbf92
Jiang-Xiao-Bai <1536907789@qq.com>
2020-05-02 17:18:03 +0800

    爬取天气案例

--- springboot/src/test/java/com/ssm/springboot/DemoTests/JSoupTest.java
+++ springboot/src/test/java/com/ssm/springboot/DemoTests/JSoupTest.java
@@ -2,13 +2,13 @@ package com.ssm.springboot.DemoTests;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * 去除String中的HTML代码
@@ -19,7 +19,7 @@ import java.util.List;
 @Slf4j
 public class JSoupTest {
 
-    public static void deleteHtmlDemo(){
+    public static void deleteHtmlDemo() {
         String html = "<html><head><title>Website title</title></head><body><p>Sample paragraph number 1 </p><p>Sample paragraph number 2</p></body></html>";
         Document doc = Jsoup.parse(html);
         System.out.println(doc.title());
@@ -29,22 +29,47 @@ public class JSoupTest {
         }
     }
 
-    public static void getDataByIntent(){
-        final String url="https://www.cnblogs.com/javazhiyin/p/11841374.html";
+    public static void getDataByIntent() {
+        final String url = "https://www.cnblogs.com/javazhiyin/p/11841374.html";
         try {
-            Document document= Jsoup.connect(url).timeout(5000).get();
-            Elements elements=document.getElementById("cnblogs_post_body").getElementsByTag("p").after("&nbsp;");
+            Document document = Jsoup.connect(url).timeout(5000).get();
+            Elements elements = document.getElementById("cnblogs_post_body").getElementsByTag("p").after("&nbsp;");
 //            for (Element element : elements) {
 //                log.info("element对象："+element.text());
 //            }
             System.out.println(elements.get(1).text());
             System.out.println(elements.get(2).text());
         } catch (IOException e) {
-            log.info("爬取数据异常",e);
+            log.info("爬取数据异常", e);
         }
     }
 
+    public static void getWeather(String areaStr) {
+        try {
+            final String areaStrUrl = "http://www.baidu.com/baidu?tn=monline_3_dg&ie=utf-8&wd=" + areaStr +"天气";
+            String url = Jsoup.connect(areaStrUrl).timeout(3000).get().select(".t").
+                    get(0).getElementsByTag("a").attr("href");
+            log.info("爬取地址："+url);
+            Document document = Jsoup.connect(url).timeout(3000).get();
+            Elements areaElements = document.select(".ctop > .crumbs");
+            Elements weaherElements = document.getElementById("7d").select(".sky");
+            for (Element element : weaherElements) {
+                System.out.println("-------------------------------------");
+                System.out.println(areaElements.get(0).getElementsByTag("a").get(2).text());
+                String dateStr=element.getElementsByTag("h1").text();
+                System.out.println(StringUtils.substringBefore(dateStr,"日"));
+                System.out.println(element.select(".wea").text());
+                System.out.println(element.select(".tem").text());
+                System.out.println(element.select(".win").text());
+            }
+        } catch (Exception e) {
+            log.info("爬取数据异常",e);
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        getDataByIntent();
+        getWeather("西安");
     }
 }
